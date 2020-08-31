4d07436d286bcb6f216a8a24eaeae57e7a3a638d
AlexAnarcho <alexanarcho@protonmail.com>
2020-08-31 19:36:14 +0400

    Added GitHub Logo + Link to Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,35 @@
 import React from "react";
 import monero from "../images/monero-symbol.png";
+import github from "../images/github-logo.png";
 
 function Footer() {
   return (
-    <footer className="absolute bottom-0 bg-gray-400 w-full py-5">
-      <a
-        href="https://www.getmonero.org/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img
-          className="h-16 object-contain mx-auto"
-          src={monero}
-          alt="Monero Logo"
-        />
-      </a>
-      <p className="text-center">This is my Footer</p>
+    <footer className="absolute bottom-0 bg-xmrgray-lighter w-full py-5">
+      <div className="flex justify-center">
+        <a
+          href="https://www.getmonero.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="inline-block h-16 object-contain mx-3"
+            src={monero}
+            alt="Monero Logo"
+          />
+        </a>
+        <a
+          href="https://github.com/hundehausen/tipxmr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            className="inline-block h-16 object-contain mx-3"
+            src={github}
+            alt="GitHub Logo"
+          />
+        </a>
+      </div>
+      <p className="text-center text-white">Find out more</p>
     </footer>
   );
 }
