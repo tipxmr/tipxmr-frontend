a495e22c034a5418c6f79d0fd2e5b636afe9b099
AlexAnarcho <alexanarcho@protonmail.com>
2020-09-01 16:40:18 +0400

    Animation

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -2,10 +2,12 @@ import React from "react";
 
 function Animation() {
   return (
-    <div>
-      <p></p>
-      <p></p>
-      <p></p>
+    <div className="text-4xl bg-opacity-0">
+      <p className="text-6xl">Donor name donated XMR Amount XMR</p>
+      <p>
+        Yo here is my message, you da best streamer. been following you since
+        forever!!!
+      </p>
     </div>
   );
 }
