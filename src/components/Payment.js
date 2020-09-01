31bfc25a19f710af5c20f6a3b1f06b5c76ac536c
AlexAnarcho <alexanarcho@protonmail.com>
2020-09-01 15:16:14 +0400

    Small Changes in formatting on Payment site

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -4,13 +4,14 @@ import ReactLoading from "react-loading";
 
 function Payment(props) {
   const amount = "1337";
+  const address = "l;kajdsflkjsdaf;l";
   return (
     <div className="mt-24 text-center">
-      <span className="font-black">{props.donor}</span>
-      <br /> send <span className="font-black">{amount} XMR</span> to
+      <span className="text-3xl">{props.donor},</span>
+      <br /> send <span className="">{amount} XMR</span> to
       <img className="mx-auto w-270px h-auto" src={testqr} alt="qr code" />
-      <pre className="text-center overlfow-x-auto text-xs">{}</pre>
-      <h2 className="mt-4 font-semibold">Your Message will be:</h2>
+      <pre className="text-center overlfow-x-auto text-xs">{address}</pre>
+      <h2 className="mt-4">Your Message will be:</h2>
       <span className="italic">{props.message}</span>
       <div className="flex justify-center mt-6">
         <ReactLoading type="spinningBubbles" color="#F16822" />
