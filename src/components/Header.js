ec782647a76fa6efd40acca0a10144064ef50223
AlexAnarcho <alexanarcho@protonmail.com>
2020-09-02 13:44:04 +0400

    Remove old header

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,6 @@ function Header() {
   return (
     <header className="bg-xmrgray-lighter p-4">
       <Nav />
-      <h2 className="text-center text-white">
-        Tip XMR to your favorite Streamer - LIVE!
-      </h2>
     </header>
   );
 }
