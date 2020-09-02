12d18bfea98d2fd5562dd4ad422009c1304fe8b8
AlexAnarcho <alexanarcho@protonmail.com>
2020-09-02 13:46:54 +0400

    Final touches on navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,8 +7,8 @@ function Nav() {
     <nav className="text-white">
       <div className="container mx-auto px-6 py-3 md:flex md:justify-between md:items-center">
         <div className="flex justify-between items-center">
-          <div>
-            <Link className="md:text-2xl " to="/">
+          <div className="mb-3 md:mb-0">
+            <Link to="/">
               <img
                 src={logo}
                 alt="tipxmr logo"
@@ -23,41 +23,34 @@ function Nav() {
           <div className="flex flex-col md:flex-row md:mx-6">
             <Link
               to="/payment"
-              className="my-2 md:my-0 md:mx-3 hover:text-xmrorange"
+              className="my-1 md:my-0 md:mx-3 hover:text-xmrorange"
             >
               Payment
             </Link>
             <Link
               to="/success"
-              className="my-2 md:my-0 md:mx-3 hover:text-xmrorange"
+              className="my-1 md:my-0 md:mx-3 hover:text-xmrorange"
             >
               Success
             </Link>
             <Link
               to="/createwallet"
-              className="my-2 md:my-0 md:mx-3 hover:text-xmrorange"
+              className="my-1 md:my-0 md:mx-3 hover:text-xmrorange"
             >
               CreateWallet
             </Link>
             <Link
               to="/openwallet"
-              className="my-2 md:my-0 md:mx-3 hover:text-xmrorange"
+              className="my-1 md:my-0 md:mx-3 hover:text-xmrorange"
             >
               OpenWallet
             </Link>
             <Link
               to="/animation"
-              className="my-2 md:my-0 md:mx-3 hover:text-xmrorange"
+              className="my-1 md:my-0 md:mx-3 hover:text-xmrorange"
             >
               Animation
             </Link>
-
-            <a
-              className="my-1 text-sm text-gray-700 font-medium hover:text-indigo-500 md:mx-4 md:my-0"
-              href="#"
-            >
-              About
-            </a>
           </div>
         </div>
       </div>
