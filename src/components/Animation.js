29ed29d3200a7209feafecc4f230c8a42c9cc347
AlexAnarcho <alex.t.roos@gmail.com>
2020-09-07 17:48:17 +0400

    Animation automatically disappears after X seconds

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -5,38 +5,44 @@ import ring from "../sounds/one-ring.mp3";
 const Sound = () => {
   return <audio src={ring} autoPlay={true}></audio>;
 };
+
 function Animation() {
+  // in milliseconds, one second costs 0.00043 xmr
+  const timeout = 2325581;
   const [donor, setDonor] = useState("AlexAnarcho");
-  const [amount, setAmount] = useState("420");
+  const [amount, setAmount] = useState(0.00172);
   const [message, setMessage] = useState("Testing things out");
   const [showMessage, setShowMessage] = useState(false);
   const messageTransitions = useTransition(showMessage, null, {
-    from: { position: "absolute", opacity: 0 },
+    from: { opacity: 0 },
     enter: { opacity: 1 },
     leave: { opacity: 0 },
   });
+  function dismountMessage() {
+    setTimeout(() => setShowMessage(false), timeout * amount);
+  }
   return (
-    <div>
-      <div className="flex flex-grow justify-center">
-        <div className="my-auto">
-          <button onClick={() => setShowMessage(!showMessage)}>
-            Show Animation
-          </button>
-          <div className="text-4xl bg-opacity-0 pl-8">
-            {messageTransitions.map(
-              ({ item, key, props }) =>
-                item && (
-                  <animated.div key={key} style={props} className="text-6xl">
-                    <Sound />
-                    <p>
-                      {donor} donated {amount} XMR
-                    </p>
-                    <p>{message}</p>
-                  </animated.div>
-                )
-            )}
-          </div>
-        </div>
+    <div
+      className="flex flex-grow justify-center bg-opacity-0"
+      onClick={() => {
+        setShowMessage(!showMessage);
+        dismountMessage();
+      }}
+    >
+      <div className="my-auto text-4xl text-center">
+        {messageTransitions.map(
+          ({ item, key, props }) =>
+            item && (
+              <animated.div key={key} style={props}>
+                <Sound />
+                <p>
+                  {donor} donated {amount} XMR
+                </p>
+
+                <p>{message}</p>
+              </animated.div>
+            )
+        )}
       </div>
     </div>
   );
