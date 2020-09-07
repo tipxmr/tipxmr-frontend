8dc6f993ad1b6f55886bb7bc09d4cfe1425404ea
AlexAnarcho <alex.t.roos@gmail.com>
2020-09-07 13:32:41 +0400

    Reworking Animation and sync status button

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Animation() {
+  const [donor, setDonor] = useState("AlexAnarcho");
+  const [amount, setAmount] = useState("420");
+  const [message, setMessage] = useState("Testing things out");
   return (
     <div className="flex flex-grow justify-center">
       <div className="my-auto">
         <div className="text-4xl bg-opacity-0 pl-8">
-          <p className="text-6xl">Donor name donated XMR Amount XMR</p>
-          <p>
-            Yo here is my message, you da best streamer. been following you
-            since forever!!!
+          <p className="text-6xl">
+            {donor} donated {amount} XMR
           </p>
+          <p>{message}</p>
         </div>
       </div>
     </div>
