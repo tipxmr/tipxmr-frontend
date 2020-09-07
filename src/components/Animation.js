bdb732c5c4646a70963bb6a18b27b837d8ac3fdc
AlexAnarcho <alex.t.roos@gmail.com>
2020-09-07 14:01:05 +0400

    Installed use-sound

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -1,17 +1,37 @@
 import React, { useState } from "react";
+import { useTransition, animated } from "react-spring";
 
 function Animation() {
   const [donor, setDonor] = useState("AlexAnarcho");
   const [amount, setAmount] = useState("420");
   const [message, setMessage] = useState("Testing things out");
+  const [showMessage, setShowMessage] = useState(false);
+  const messageTransitions = useTransition(showMessage, null, {
+    from: { position: "absolute", opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
   return (
-    <div className="flex flex-grow justify-center">
-      <div className="my-auto">
-        <div className="text-4xl bg-opacity-0 pl-8">
-          <p className="text-6xl">
-            {donor} donated {amount} XMR
-          </p>
-          <p>{message}</p>
+    <div>
+      <button onClick={() => setShowMessage(!showMessage)}>
+        Show animation
+      </button>
+
+      <div className="flex flex-grow justify-center">
+        <div className="my-auto">
+          <div className="text-4xl bg-opacity-0 pl-8">
+            {messageTransitions.map(
+              ({ item, key, props }) =>
+                item && (
+                  <animated.div key={key} style={props} className="text-6xl">
+                    <p>
+                      {donor} donated {amount} XMR
+                    </p>
+                    <p>{message}</p>
+                  </animated.div>
+                )
+            )}
+          </div>
         </div>
       </div>
     </div>
