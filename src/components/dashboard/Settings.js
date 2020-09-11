11816727e1cf597341ef9137ed5a06947345383a
AlexAnarcho <alex.t.roos@gmail.com>
2020-09-11 15:44:54 +0400

    Setting up a component for inputs

--- src/components/dashboard/Settings.js
+++ src/components/dashboard/Settings.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
+import InputField from "../dump_components/InputField";
 
 function Settings({ streamerConfig, setStreamerConfig }) {
   // TODO state for every config option
@@ -10,19 +11,26 @@ function Settings({ streamerConfig, setStreamerConfig }) {
 
   function updateStreamerConfig() {
     // TODO read states and update streamerConfig object
+    // use setStreamerConfig, change triggers useEffect that triggers socket
   }
   return (
-    <div>
-      <input
-        placeholder={streamerConfig.streamerName}
-        onChange={(e) => {
-          setStreamerConfig({
-            ...streamerConfig,
-            streamerName: e.target.value,
-          });
-          console.log(streamerConfig.streamerName);
-        }}
-      ></input>
+    <div className="h-full">
+      <div className="text-center text-xl underline mb-4">
+        Change your Settings:
+      </div>
+      <label>
+        Your displayName:
+        <input
+          placeholder={streamerConfig.streamerName}
+          onChange={(e) => {
+            setStreamerConfig({
+              ...streamerConfig,
+              streamerName: e.target.value,
+            });
+            console.log(streamerConfig.streamerName);
+          }}
+        ></input>
+      </label>
     </div>
   );
 }
