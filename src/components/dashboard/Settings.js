b699d6951aee28e72e02bfe242b49f5fe1b685a1
AlexAnarcho <alex.t.roos@gmail.com>
2020-09-11 16:55:46 +0400

    Intregrated Savebutton mockup

--- src/components/dashboard/Settings.js
+++ src/components/dashboard/Settings.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import InputField from "../dump_components/InputField";
+import Savebutton from "../dump_components/Savebutton";
 
 function Settings({ streamerConfig, setStreamerConfig }) {
   // TODO state for every config option
@@ -27,33 +28,36 @@ function Settings({ streamerConfig, setStreamerConfig }) {
   return (
     <div className="h-full text-xmrgray-darker">
       <div className="m-3 w-3/4 mx-auto bg-gray-200">Testing</div>
-      <div className="text-center text-xl underline mb-4">
-        Change your Settings:
+      <div>
+        <div className="text-center text-xl underline mb-4">
+          Change your Settings:
+        </div>
+        <InputField
+          configKey="displayName"
+          labelName="Change your display name"
+          placeholderName={displayName}
+          fieldType="text"
+        />
+        <InputField
+          configKey="streamURLS.twitch"
+          labelName="Set URL to your stream"
+          placeholderName={streamURLS.twitch}
+          fieldType="text"
+        />
+        <InputField
+          configKey="restoreHeight"
+          labelName="Restore Height for Wallet"
+          placeholderName={restoreHeight}
+          fieldType="text"
+        />
+        <InputField
+          configKey="profilePicture"
+          labelName="Change your profile picture"
+          placeholderName={profilePicture}
+          fieldType="image"
+        />
       </div>
-      <InputField
-        configKey="displayName"
-        labelName="Change your display name"
-        placeholderName={displayName}
-        fieldType="text"
-      />
-      <InputField
-        configKey="streamURLS.twitch"
-        labelName="Set URL to your stream"
-        placeholderName={streamURLS.twitch}
-        fieldType="text"
-      />
-      <InputField
-        configKey="restoreHeight"
-        labelName="Restore Height for Wallet"
-        placeholderName={restoreHeight}
-        fieldType="text"
-      />
-      <InputField
-        configKey="profilePicture"
-        labelName="Change your profile picture"
-        placeholderName={profilePicture}
-        fieldType="image"
-      />
+      <Savebutton />
     </div>
   );
 }
