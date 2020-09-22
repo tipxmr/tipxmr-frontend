3bce041c970d8b208e2b1713bb1941ccef9d52d2
Jonas Hauß <haussjonas@tutanota.de>
2020-09-22 11:42:34 +0200

    switch styles to dynamic classnames

--- src/components/IsOnlineBadge.js
+++ src/components/IsOnlineBadge.js
@@ -1,31 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
+import clsx from "clsx";
 
 function IsOnlineBadge({ isOnline }) {
-  function onlineBadge() {
-    return (
-      <button
-        type="button"
-        className="pointer-events-none mx-auto bg-green-600 text-white p-2 rounded leading-none flex items-center"
-      >
-        online
-      </button>
-    );
-  }
-
-  function offlineBadge() {
-    return (
-      <button
-        type="button"
-        className="pointer-events-none mx-auto bg-red-600 text-white p-2 rounded leading-none flex items-center"
-      >
-        offline
-      </button>
-    );
-  }
-
-  return <div>{isOnline ? onlineBadge() : offlineBadge()}</div>;
+  return (
+    <button
+      type="button"
+      className={clsx([
+        "pointer-events-none",
+        "mx-auto",
+        "text-white",
+        "p-2",
+        "rounded",
+        "leading-none",
+        "flex",
+        "items-center",
+        {
+          "bg-green-600": isOnline,
+          "bg-red-600": !isOnline,
+        },
+      ])}
+    >
+      {isOnline ? "online" : "offline"}
+    </button>
+  );
 }
+
 IsOnlineBadge.propTypes = {
   isOnline: PropTypes.bool,
 };
