4dbe6d60368bf69f417e31c931825cd666db72a4
Jonas Hauß <haussjonas@tutanota.de>
2020-09-22 11:15:19 +0200

    add hook for block height notification

---
+++ src/hook/useBlockHeight.js
@@ -0,0 +1,49 @@
+import { useEffect, useRef, useState } from "react";
+import { MoneroWalletListener } from "monero-javascript";
+
+import { useWalletState } from "../context/wallet";
+
+class BlockHeightListener extends MoneroWalletListener {
+  constructor(onBlockHeight) {
+    super();
+    this.onBlockHeight = onBlockHeight;
+  }
+
+  onNewBlock(height) {
+    this.onBlockHeight(height);
+  }
+}
+
+export function useBlockHeight() {
+  const wallet = useWalletState();
+  const listenerRef = useRef();
+  const [blockHeight, setBlockHeight] = useState(0);
+
+  function onBlockHeight(height) {
+    setBlockHeight(height);
+  }
+
+  useEffect(() => {
+    listenerRef.current = new BlockHeightListener(onBlockHeight);
+
+    return () => {
+      listenerRef.current = null;
+    };
+  }, []);
+
+  useEffect(() => {
+    if (wallet.wallet) {
+      wallet.wallet.addListener(listenerRef.current);
+    }
+
+    return () => {
+      if (wallet.wallet) {
+        wallet.wallet.removeListener(listenerRef.current);
+      }
+    };
+  }, [wallet.wallet]);
+
+  return blockHeight;
+}
+
+export default useBlockHeight;
