290d1e83ddca397e11842e9d6173b4b475aedd2f
Jonas Hauß <haussjonas@tutanota.de>
2020-09-22 11:48:12 +0200

    unsued code removed. rework to use context

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Route, BrowserRouter as Router } from "react-router-dom";
+import PropTypes from "prop-types";
+import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
 import monerojs from "./libs/monero";
 import socketio from "./libs/socket";
 
@@ -8,30 +9,27 @@ import * as WalletContext from "./context/wallet";
 import {
   Header,
   Footer,
-  Start,
   Donate,
   Login,
-  CreateWallet,
-  OpenWallet,
   Animation,
   Dashboard,
   Disclaimer,
   FAQ,
+  Logout,
+  PrivateRoute,
 } from "./components";
 
+import useIncomingTransaction from "./hook/useIncomingTransaction";
+
 function App() {
+  useIncomingTransaction(onIncomingTransaction);
+
   let walletUseEffectDidFire = false;
-  const [wallet, setWallet] = useState(null);
-  const [primaryAddress, setPrimaryAddress] = useState(null);
-  const [currentSyncBlockheight, setCurrentSyncBlockheight] = useState(null);
-  const [percentageSynced, setPercentageSynced] = useState(0);
-  const [isSyncActive, setIsSyncActive] = useState(false);
   const [donorInfo, setDonorInfo] = useState([]);
   const [donationsQueue, setDonationsQueue] = useState([]);
   const [donationsHistory, setDonationsHistory] = useState([]);
 
   const [customWallet, dispatch] = WalletContext.useWallet();
-  console.log("customWallet", customWallet);
 
   const [streamerConfig, setStreamerConfig] = useState({
     hashedSeed: "", // acts as password for login
@@ -88,11 +86,6 @@ function App() {
     },
   });
 
-  // FIXME: move wallet handling into context completely
-  useEffect(() => {
-    dispatch({ type: "SET_WALLET", wallet });
-  }, [wallet]);
-
   useEffect(() => {
     if (streamerConfig && streamerConfig.restoreHeight) {
       dispatch({
@@ -102,20 +95,14 @@ function App() {
     }
   }, [streamerConfig.restoreHeight]);
 
-  /* Example for donorInfo object: {
-    donatorSocketId: "5NXW3Rj1eKRqjE9sAAOw"
-    donor: "Grischa"
-    hashedSeed: null
-    message: "Test 6"
-    displayName: "MoneroMumble"
-    subaddress: "76ABPQ3e2GmVAkDf7BQwXcFb3QCfwG2osQpJo8J3WVVoa4ZrXzPxoEm9fPq7nHdFJMZ32q7B5qGNbBJCiaSBzSAJ1wgFwJi"
-    }
-    */
+  function onIncomingTransaction(tx) {
+    getNewOutput(tx);
+  }
 
   function getNewOutput(output) {
     console.log("getNewOutput aufgerufen, output:", output);
     monerojs
-      .getSubaddress(wallet, output.subaddressIndex)
+      .getSubaddress(customWallet.wallet, output.subaddressIndex)
       .then((subaddress) => {
         console.log("Donation an diese Subaddresse:", subaddress);
         console.log("donorInfo:", donorInfo);
@@ -147,24 +134,8 @@ function App() {
       });
   }
 
-  const mwl = new monerojs.MyWalletListener(
-    setPercentageSynced,
-    setCurrentSyncBlockheight,
-    getNewOutput
-  );
-
-  async function syncWallet() {
-    setIsSyncActive(true);
-    monerojs
-      .startSyncing(wallet, mwl, streamerConfig.restoreHeight)
-      .catch((err) => {
-        console.error(err);
-        setIsSyncActive(false);
-      });
-  }
-
   function handleOnNewSubaddress(data) {
-    monerojs.createSubaddress(wallet).then((subaddress) => {
+    monerojs.createSubaddress(customWallet.wallet).then((subaddress) => {
       const newDonorInfo = { ...data, subaddress: subaddress };
       setDonorInfo((previousArray) => [...previousArray, newDonorInfo]);
       socketio.emitSubaddressToBackend(newDonorInfo);
@@ -174,50 +145,19 @@ function App() {
 
   // as soon as wallet is loaded
   useEffect(() => {
-    if (wallet !== null && walletUseEffectDidFire === false) {
+    if (customWallet.wallet && walletUseEffectDidFire === false) {
       // after login send streamer info
       socketio.emitStreamerInfo(streamerConfig);
       // listen for new request of subaddress generation
       socketio.onCreateSubaddress(handleOnNewSubaddress);
       walletUseEffectDidFire = true;
     }
-  }, [wallet, walletUseEffectDidFire]);
+  }, [customWallet.wallet, walletUseEffectDidFire]);
 
   // handleChange for userConfig
   useEffect(() => {
     socketio.emitUpdateStreamerConfig(streamerConfig);
   }, [streamerConfig]);
-
-  useEffect(() => {
-    console.log("isSyncActive: ", isSyncActive);
-  }, [isSyncActive]);
-
-  // New Block is added to the chain
-  useEffect(() => {
-    console.log(
-      "New Block (" + currentSyncBlockheight + ") added to the blockchain."
-    );
-  }, [currentSyncBlockheight]);
-
-  function getWalletFunctions() {
-    return {
-      setStreamerConfig,
-      setIsSyncActive,
-      syncWallet,
-      setWallet,
-      setPrimaryAddress,
-    };
-  }
-
-  function getWalletVariables() {
-    return {
-      isSyncActive,
-      streamerConfig,
-      wallet,
-      primaryAddress,
-      percentageSynced,
-    };
-  }
 
   return (
     <div className="flex flex-col min-h-screen">
@@ -226,41 +166,34 @@ function App() {
         <div className="flex-auto flex flex-col">
           <div className="flex flex-full">
             <Route path="/" exact>
-              <Start />
+              <Redirect to="/dashboard" />
             </Route>
             <Route path="/donate/:userName">
               <Donate />
             </Route>
-            <Route>
-              <Login />
-            </Route>
-            <Route path="/createwallet" exact>
-              <CreateWallet />
-            </Route>
-            <Route path="/openwallet" exact>
-              <OpenWallet
+            <Route path="/login">
+              <Login
                 streamerConfig={streamerConfig}
                 setStreamerConfig={setStreamerConfig}
-                walletFunctions={getWalletFunctions()}
-                walletVariables={getWalletVariables()}
               />
             </Route>
             <Route path="/animation" exact>
               <Animation streamerConfig={streamerConfig} />
             </Route>
-            <Route path="/dashboard">
+            <PrivateRoute path="/dashboard">
               <Dashboard
-                walletFunctions={getWalletFunctions()}
-                walletVariables={getWalletVariables()}
                 streamerConfig={streamerConfig}
                 setStreamerConfig={setStreamerConfig}
               />
-            </Route>
+            </PrivateRoute>
             <Route path="/disclaimer">
               <Disclaimer />
             </Route>
             <Route path="/faq">
               <FAQ />
+            </Route>
+            <Route path="/logout">
+              <Logout />
             </Route>
           </div>
         </div>
