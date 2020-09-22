73f39960eb40745efcc7a7f6469cefffc6bafbf4
Jonas Hauß <haussjonas@tutanota.de>
2020-09-22 11:44:56 +0200

    move create/open wallet components into login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,39 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { Link, useRouteMatch, Route } from "react-router-dom";
 import { CreateWallet, OpenWallet } from "./";
 
-function Login() {
-  return <div></div>;
+function Login({ streamerConfig, setStreamerConfig }) {
+  const { path, url } = useRouteMatch();
+
+  return (
+    <Fragment>
+      <Route path={path} exact>
+        <div className="flex flex-grow justify-center">
+          <div className="my-auto">
+            <Link to={`${url}/create`}>
+              <button className="bg-xmrorange hover:bg-xmrorange-darker text-white font-bold my-16 mx-8 py-2 px-4 rounded">
+                Create new wallet
+              </button>
+            </Link>
+            <Link to={`${url}/open`}>
+              <button className="bg-xmrorange hover:bg-xmrorange-darker text-white font-bold my-16 mx-8 py-2 px-4 rounded">
+                Open existing wallet
+              </button>
+            </Link>
+          </div>
+        </div>
+      </Route>
+      <Route path={`${path}/create`}>
+        <CreateWallet />
+      </Route>
+      <Route path={`${path}/open`}>
+        <OpenWallet
+          streamerConfig={streamerConfig}
+          setStreamerConfig={setStreamerConfig}
+        />
+      </Route>
+    </Fragment>
+  );
 }
 
 export default Login;
