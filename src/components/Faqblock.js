f4e73fdbdb63f5f9ddbdcaf4c526b5f555a7d162
Jonas Hauß <haussjonas@tutanota.de>
2020-09-24 13:32:21 +0200

    fix animation issue while closing the answer

--- src/components/Faqblock.js
+++ src/components/Faqblock.js
@@ -1,49 +1,35 @@
 import React, { useState, useEffect } from "react";
+import { TiArrowDownThick, TiWeatherDownpour } from "react-icons/ti";
 import { useSpring, animated } from "react-spring";
 import { useMeasure } from "react-use";
 import clsx from "clsx";
-import { TiArrowDownThick } from "react-icons/ti";
-import { IconContext } from "react-icons";
 
-function Faqblock({ question, children }) {
-  // answer show on isOpen
-  const [isOpen, setIsOpen] = useState(false);
+// --- QUESTION ---
+function Question({ question, isOpen }) {
+  const arrowStyles = clsx([
+    "transition",
+    "transform",
+    "duration-200",
+    "ease-in-out",
+    "white",
+    {
+      "rotate-180": isOpen, // magic happens here
+    },
+  ]);
+
+  return (
+    <div className="p-4 pl-8 text-xl text-white flex justify-between">
+      <p>{question}</p>
+      <TiArrowDownThick className={arrowStyles} color="white" size="1.5em" />
+    </div>
+  );
+}
+
+// ---ANSWER-- -
+function Answer({ children, isOpen }) {
   const defaultHeight = "1px";
   const [contentHeight, setContentHeight] = useState(defaultHeight);
   const [ref, { height }] = useMeasure();
-
-  // --- QUESTION ---
-  function Question(question) {
-    const arrowStyles = clsx([
-      "transition",
-      "transform",
-      "duration-200",
-      "ease-in-out",
-      "white",
-      {
-        "rotate-180": isOpen, // magic happens here
-      },
-    ]);
-
-    return (
-      <div className="p-4 pl-8 text-xl text-white flex justify-between">
-        <p>{question}</p>
-        <TiArrowDownThick className={arrowStyles} color="white" size="1.5em" />
-      </div>
-    );
-  }
-
-  // --- ANSWER ---
-  function Answer(answer) {
-    return (
-      <animated.div
-        style={expand}
-        className="p-4 border-4 border-xmrgray-lighter bg-gray-200 border-t-0"
-      >
-        <div ref={ref}>{children}</div>
-      </animated.div>
-    );
-  }
 
   // Answer animation
   const expand = useSpring({
@@ -63,12 +49,27 @@ function Faqblock({ question, children }) {
   }, [height]);
 
   return (
+    <animated.div
+      style={expand}
+      className="border-4 border-xmrgray-lighter bg-gray-200 border-t-0 overflow-hidden"
+    >
+      <div className="p-4" ref={ref}>
+        {children}
+      </div>
+    </animated.div>
+  );
+}
+
+function Faqblock({ question, children }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
     <div
       onClick={() => setIsOpen(!isOpen)}
       className="bg-xmrgray-lighter my-3 transition duration-500"
     >
-      {Question(question)}
-      {isOpen ? Answer(children) : ""}
+      <Question question={question} isOpen={isOpen} />
+      <Answer isOpen={isOpen}>{children}</Answer>
     </div>
   );
 }
