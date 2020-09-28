2cd927134e05a7d1ce61acc056817b2023f76ee3
hundehausen <mail@grischadaum.de>
2020-09-28 13:03:12 +0200

    added Streamer Page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
 import { Route, BrowserRouter as Router, Redirect } from "react-router-dom";
 import monerojs from "./libs/monero";
 import socketio from "./libs/socket";
