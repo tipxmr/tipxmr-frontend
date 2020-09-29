27b29c34092b0b383f98176d63f3bb5e97ed2da6
AlexAnarcho <alexanarcho@protonmail.com>
2020-09-29 10:47:59 +0400

    StreamerNotFound Page added

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,4 +8,5 @@ export { default as OpenWallet } from "./OpenWallet";
 export { default as Animation } from "./Animation";
 export { default as Dashboard } from "./Dashboard";
 export { default as StreamerPage } from "./StreamerPage";
+export { default as StreamerNotFound } from "./StreamerNotFound";
 export { default as Logout } from "./Logout";
