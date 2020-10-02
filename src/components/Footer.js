acff4fbf5ae6ba2ff65c089e96680e4b8b72b7af
AlexAnarcho <alexanarcho@protonmail.com>
2020-10-02 12:51:39 +0400

    Rework of Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,37 +1,27 @@
 import React from "react";
-import monero from "../images/monero-symbol.png";
-import github from "../images/github-logo.png";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
-    <footer className="bg-xmrgray-lighter w-full py-3">
-      <div className="flex justify-center mb-3">
-        <a
-          href="https://www.getmonero.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img
-            className="inline-block h-16 object-contain mx-3 transform hover:scale-105"
-            src={monero}
-            alt="Monero Logo"
-          />
+    <footer className="text-white text-sm bg-xmrgray-lighter w-full py-3">
+      <div className="flex justify-around">
+        <Link to="/disclaimer" className="hover:text-xmrorange">
+          Disclaimer
+        </Link>
+        <Link to="/faq" className="hover:text-xmrorange">
+          FAQ
+        </Link>
+
+        <a href="https://getmonero.org" className="hover:text-xmrorange">
+          GetMonero
         </a>
         <a
           href="https://github.com/hundehausen/tipxmr"
           target="_blank"
           rel="noopener noreferrer"
+          className="hover:text-xmrorange"
         >
-          <img
-            className="inline-block h-16 object-contain mx-3 transform hover:scale-105"
-            src={github}
-            alt="GitHub Logo"
-          />
-        </a>
-      </div>
-      <div className="text-center text-white text-xl">
-        <a href="/disclaimer" className="hover:text-xmrorange">
-          Disclaimer
+          GitHub
         </a>
       </div>
     </footer>
