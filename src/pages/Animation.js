55459cb721ce0c5f923486ba7603312211a78ba0
AlexAnarcho <alexanarcho@protonmail.com>
2020-10-28 13:42:39 +0400

    Fixing the Animation Components

--- src/pages/Animation.js
+++ src/pages/Animation.js
@@ -8,72 +8,114 @@ import {
   emitGetAnimationConfig,
   onGetAnimationConfig,
 } from "~/libs/socket_animation";
+import clsx from "clsx";
+
+function GoalBar({ goalProgress, goal }) {
+  const barStyle = clsx([
+    "bg-xmrorange",
+    "text-xs",
+    "leading-none",
+    "py-1",
+    "text-center",
+    "text-white",
+    { width: (goalProgress / goal) * 100 + "%" },
+  ]);
+  return (
+    <div className="mt-3">
+      <div className="w-full">
+        <div className="shadow-lg w-full">
+          <div className={barStyle}>{(goalProgress / goal) * 100}%</div>
+          {console.log(goalProgress)}
+        </div>
+      </div>
+      <p className="text-xl">
+        Goal: {goalProgress}/{goal} XMR
+      </p>
+    </div>
+  );
+}
+
+function Sound({ sound }) {
+  return <audio src={sound} autoPlay={true}></audio>;
+}
 
 function Animation() {
   const { userName } = useParams();
   const [animationConfig, setAnimationConfig] = useState();
+  const [time, setTime] = useState(0);
+  const [fontColor, setFontColor] = useState(null);
+  const [showGoal, setShowGoal] = useState(false);
+  const [goalProgress, setGoalProgress] = useState(0);
+  const [sound, setSound] = useState(null);
+  const [goal, setGoal] = useState(0);
+
+  const [donor, setDonor] = useState("Happy Tipper");
+  const [amount, setAmount] = useState(2);
+  const [message, setMessage] = useState("Testing things out");
+  const [showMessage, setShowMessage] = useState(false);
 
   useEffect(() => {
     emitGetAnimationConfig(userName);
     onGetAnimationConfig(setAnimationConfig);
   }, [userName]);
 
-  const streamerConfig = useRecoilValue(streamerState);
+  useEffect(() => {
+    if (animationConfig) {
+      setTime(1000 / animationConfig.secondPrice);
+      setFontColor(animationConfig.fontColor);
+      setShowGoal(animationConfig.showGoal);
+      setGoalProgress(animationConfig.goalProgress);
+      setSound(animationConfig.sound);
+      setGoal(animationConfig.goal);
+      setTimeout(() => {
+        setGoalProgress((prevState) => prevState + amount);
+      }, 2000);
+    }
+  }, [animationConfig]);
 
-  // old states for the streamer animation
-  // in milliseconds, one second costs 0.00043 xmr
-  // const timeout = 1000 / streamerConfig.animationSettings.secondPrice;
-  // const fontcolor = streamerConfig.animationSettings.fontColor;
-  // const goalprogress = streamerConfig.animationSettings.goalProgress;
-  // const sound = streamerConfig.animationSettings.sound;
-  // const goal = streamerConfig.animationSettings.goal;
-  // const [donor, setDonor] = useState("AlexAnarcho");
-  // const [amount, setAmount] = useState(0.0172);
-  // const [message, setMessage] = useState("Testing things out");
-  // const [showMessage, setShowMessage] = useState(false);
-  // const messageTransitions = useTransition(showMessage, null, {
-  //   from: { opacity: 0 },
-  //   enter: { opacity: 1 },
-  //   leave: { opacity: 0 },
-  // });
-  // const Sound = () => {
-  //   return <audio src={sound} autoPlay={true}></audio>;
-  // };
+  // const streamerConfig = useRecoilValue(streamerState);
+  // console.log("Animation streamer config: ", streamerConfig);
 
-  console.log("streamerConfig for animation: ", animationConfig);
-  // function dismountMessage() {
-  //   setTimeout(() => setShowMessage(false), timeout * amount);
-  // }
+  const messageTransitions = useTransition(showMessage, null, {
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
+  });
+
+  function dismountMessage() {
+    setTimeout(() => setShowMessage(false), time * amount);
+  }
+
+  // TODO create a template for the most basic donation
+  // TODO implement the goal, show only if the config says so
+
   return (
     <div
-    /* className="flex flex-grow justify-center bg-opacity-0" */
-    /* onClick={() => { */
-    /*   setShowMessage(!showMessage); */
-    /*   dismountMessage(); */
-    /* }} */
+      className="flex flex-grow justify-center bg-opacity-0"
+      onClick={() => {
+        setShowMessage(!showMessage);
+        dismountMessage();
+      }}
     >
-      {" "}
-      Hi there
-      {/* <div */}
-      {/*   className="my-auto text-4xl text-center" */}
-      {/*   /\* style={{ color: fontcolor }} *\/ */}
-      {/* > */}
-      {/*   {messageTransitions.map( */}
-      {/*     ({ item, key, props }) => */}
-      {/*       item && ( */}
-      {/*         <animated.div key={key} style={props}> */}
-      {/*           <Sound /> */}
-      {/*           <p> */}
-      {/*             {donor} donated {amount} XMR */}
-      {/*           </p> */}
-      {/*           <p>{message}</p> */}
-      {/*           <p className="text-xl"> */}
-      {/*             Goal: {goalprogress + amount}/{goal} XMR */}
-      {/*           </p> */}
-      {/*         </animated.div> */}
-      {/*       ) */}
-      {/*   )} */}
-      {/* </div> */}
+      <div className="my-auto text-4xl text-center">
+        {messageTransitions.map(
+          ({ item, key, props }) =>
+            item && (
+              <animated.div key={key} style={props}>
+                {sound ? <Sound sound={sound} /> : ""}
+                <p>
+                  {donor} donated {amount} XMR
+                </p>
+                <p>{message}</p>
+                {showGoal ? (
+                  <GoalBar goalProgress={goalProgress} goal={goal} />
+                ) : (
+                  ""
+                )}
+              </animated.div>
+            )
+        )}
+      </div>
     </div>
   );
 }
