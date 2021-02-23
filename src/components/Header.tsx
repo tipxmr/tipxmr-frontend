420852fa1a79a3ae0d90d95642fd6bf8f00d9e06
AlexAnarcho <alexanarcho@protonmail.com>
2021-02-23 23:53:34 +0100

    [TIPXMR-78] Header in tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import Nav from "./Nav";
-import PropTypes from "prop-types";
 
-function Header({ userName }) {
+interface HeaderI {
+  userName: string,
+}
+
+function Header({ userName }: HeaderI) {
   return (
     <header className="bg-xmrgray-darker p-4">
       <Nav userName={userName} />
     </header>
   );
 }
-Header.propTypes = {
-  userName: PropTypes.string,
-};
 
 export default Header;
