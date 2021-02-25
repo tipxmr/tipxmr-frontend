b3a92365c7f314be9c77fa4b676b0aa1e2d6a014
hundehausen <mail@grischadaum.de>
2021-02-25 21:36:42 +0100

    fixed script

--- rename_to_ts.sh
+++ rename_to_ts.sh
@@ -1,2 +1,2 @@
-find ./src -name "*.tsx" -exec sh -c 'mv "$0" "${0%.tsx}.jsx"' {} \;
-find ./src -name "*.ts" -exec sh -c 'mv "$0" "${0%.ts}.js"' {} \;
+find ./src -name "*.jsx" -exec sh -c 'mv "$0" "${0%.jsx}.tsx"' {} \;
+find ./src -name "*.js" -exec sh -c 'mv "$0" "${0%.js}.ts"' {} \;
