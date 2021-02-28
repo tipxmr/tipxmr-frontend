afd316d27f3da0fd0b91a94df939df219265f177
AlexAnarcho <alexanarcho@protonmail.com>
2021-02-28 13:53:02 +0100

    constructing the layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,25 @@
-import { Layout } from 'antd';
+import logo from "../images/tipxmr-live.png";
+import { Layout, Menu } from 'antd';
 
+const { SubMenu } = Menu;
 const { Header, Footer, Sider, Content } = Layout;
 
-function Layout() {
+function TipLayout() {
     return (
         <Layout>
-            <Header>This is a header</Header>
+            <Header className="header">
+                {/* <Title level={3}>TipXMR</Title> */}
+                <div className={logo} />
+                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                    <Menu.Item key="1">Streams</Menu.Item>
+                    <Menu.Item key="2">Donate</Menu.Item>
+                    <Menu.Item key="3">Login/Signup</Menu.Item>
+                </Menu>
+            </Header>
             <Content>This is some awesome Content</Content>
             <Footer>This is a footer</Footer>
         </Layout >
     )
 }
 
-export default Layout
+export default TipLayout
