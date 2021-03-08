b6a38abeaa4590e88e5a3cd659e2231baa83e498
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-08 12:29:41 +0200

    Change Layout of footer to space-around

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,24 +9,34 @@ const { Footer } = Layout
 function TipFooter() {
   return (
     <Footer style={{ "padding": 0, "margin": 0 }}>
-      <Row justify="center" align="middle">
-        <Col span={24}>
-          <Menu theme="dark" mode="horizontal">
+      <Menu theme="dark" mode="horizontal">
+
+        <Row justify="space-around" align="middle">
+          <Col>
             <Menu.Item key="disclaimer">
               <Link to="/disclaimer">
                 Disclaimer
               </Link>
             </Menu.Item>
+          </Col>
+
+          <Col>
             <Menu.Item key="faq">
               <Link to="/faq">
                 FAQ
               </Link>
             </Menu.Item>
+          </Col>
+          <Col>
+
             <Menu.Item key="getmonero">
               <a href="https:getmonero.org">
                 GetMonero
               </a>
             </Menu.Item>
+          </Col>
+          <Col>
+
             <Menu.Item key="github">
               <a
                 href="https:github.com/hundehausen/tipxmr"
@@ -37,9 +47,10 @@ function TipFooter() {
                 <GithubOutlined />
               </a>
             </Menu.Item>
-          </Menu>
-        </Col>
-      </Row>
+          </Col>
+        </Row>
+      </Menu>
+
     </Footer >
   );
 }
