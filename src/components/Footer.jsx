05d8b029e3c58adb1ee5beee0a0b66ee347d9301
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-08 12:16:43 +0200

    Footer as Menu

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,46 +1,42 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import clsx from "clsx";
-import { Layout, Row, Col } from 'antd'
+import { Layout, Menu, Row, Col } from 'antd'
 
 
 const { Footer } = Layout
 
 function TipFooter() {
-  // TODO eliminate clsx
-  const linkStyle = clsx([
-    "transform",
-    "hover:text-xmrorange-lighter",
-    "hover:scale-110",
-  ]);
   return (
-    <Footer style={{ "background-color": "#4D4D4D" }
-    }>
-      <Row justify="space-around" align="middle">
-        <Col>
-          <Link to="/disclaimer" className={linkStyle}>
-            Disclaimer
-        </Link>
-        </Col>
-        <Col>
-          <Link to="/faq" className={linkStyle}>
-            FAQ
-        </Link>
-        </Col>
-        <Col>
-          <a href="https:getmonero.org" className={linkStyle}>
-            GetMonero
-        </a>
-        </Col>
-        <Col>
-          <a
-            href="https:github.com/hundehausen/tipxmr"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={linkStyle}
-          >
-            GitHub
-        </a>
+    <Footer>
+      <Row justify="center" align="middle">
+        <Col span={24}>
+          <Menu theme="dark" mode="horizontal">
+            <Menu.Item key="disclaimer">
+              <Link to="/disclaimer">
+                Disclaimer
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="faq">
+              <Link to="/faq">
+                FAQ
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="getmonero">
+              <a href="https:getmonero.org">
+                GetMonero
+              </a>
+            </Menu.Item>
+            <Menu.Item key="github">
+              <a
+                href="https:github.com/hundehausen/tipxmr"
+                target="_blank"
+                rel="noopener noreferrer"
+
+              >
+                GitHub
+              </a>
+            </Menu.Item>
+          </Menu>
         </Col>
       </Row>
     </Footer >
