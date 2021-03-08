412742b919cfaa1b6bac168c8fcddb334917ef64
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-08 11:10:01 +0200

    Basic Layout mostly header

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,28 @@
 import logo from "../images/tipxmr-live.png";
-import { Layout, Menu, Image } from 'antd';
-import { tiplayout } from "../styles/tiplayout"
+import { Layout, Menu, Row, Col } from 'antd';
+// import { tiplayout } from "../styles/tiplayout"
 
 // const { SubMenu } = Menu;
-const { Header, Footer, Sider, Content } = Layout;
+const { Header, Footer, Content } = Layout;
 
 function TipLayout() {
     return (
         <Layout>
             <Header>
-                {/* <Title level={3}>TipXMR</Title> */}
-                <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
 
-                    <img src={logo} width={180} />
-                    <Menu.Item key="1">Streams</Menu.Item>
-                    <Menu.Item key="2">Donate</Menu.Item>
-                    <Menu.Item key="3">Login/Signup</Menu.Item>
-                </Menu>
+                <Row justify="space-around" align="middle">
+                    <Col>
+                        <img src={logo} width={180} />
+                    </Col>
+                    <Col>
+                        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
+                            <Menu.Item key="1">Streams</Menu.Item>
+                            <Menu.Item key="2">Donate</Menu.Item>
+                            <Menu.Item key="3">Login/Signup</Menu.Item>
+                        </Menu>
+                    </Col>
+                </Row>
+
             </Header>
             <Content>This is some awesome Content</Content>
             <Footer>This is a footer</Footer>
