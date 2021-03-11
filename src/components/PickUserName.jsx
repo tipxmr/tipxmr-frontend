042a4480cc12281f8a70bea828af15d116d774c9
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-11 14:03:09 +0200

    Tooltip

--- src/components/PickUserName.jsx
+++ src/components/PickUserName.jsx
@@ -1,4 +1,4 @@
-import { Row, Col, Typography, Input } from "antd";
+import { Row, Col, Typography, Input, Tooltip } from "antd";
 import './styles/PickUserName.css';
 
 const { Title } = Typography
@@ -9,11 +9,10 @@ const PickUserName = ({ onChange, isLoading, userNameError }) => {
             <Row justify="center" align="middle">
                 <Col span={20} style={{ "text-align": "center" }}>
                     <Title level={2}>Pick your username</Title>
-                    <Input onChange={onChange} disabled={isLoading} />
+                    <Tooltip title="This name cannot be changed later - choose wisely">
+                        <Input onChange={onChange} disabled={isLoading} />
+                    </Tooltip>
                     <p className="error-style-1">{userNameError}</p>
-                    <p className="error-style-2">
-                        This name cannot be changed once chosen
-                        </p>
 
                 </Col>
             </Row>
