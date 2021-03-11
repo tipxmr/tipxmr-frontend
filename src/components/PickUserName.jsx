fa099dccfc4e8e425cfc88d0c464b5bf54c125d2
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-11 13:58:52 +0200

    Restyle PickUserInput

--- src/components/PickUserName.jsx
+++ src/components/PickUserName.jsx
@@ -1,22 +1,24 @@
-import { Typography } from "antd";
+import { Row, Col, Typography, Input } from "antd";
 import './styles/PickUserName.css';
 
 const { Title } = Typography
 
 const PickUserName = ({ onChange, isLoading, userNameError }) => {
     return (
-        <div className="pickusername-container">
-            <Title level={2}>Pick your username</Title>
-            <input
-                className="text-xmrgray-darker p-2 rounded focus:border-none"
-                onChange={onChange}
-                disabled={isLoading}
-            ></input>
-            <p className="error-style-1">{userNameError}</p>
-            <p className="error-style-2">
-                This name cannot be changed once chosen
-            </p>
+        <div>
+            <Row justify="center" align="middle">
+                <Col span={20} style={{ "text-align": "center" }}>
+                    <Title level={2}>Pick your username</Title>
+                    <Input onChange={onChange} disabled={isLoading} />
+                    <p className="error-style-1">{userNameError}</p>
+                    <p className="error-style-2">
+                        This name cannot be changed once chosen
+                        </p>
+
+                </Col>
+            </Row>
         </div>
+
     );
 }
 
