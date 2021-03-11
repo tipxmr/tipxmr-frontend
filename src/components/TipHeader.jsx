0312fd20ee76e57a1c7b50d34e467a5a99c5fa32
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-11 10:34:33 +0200

    Added react router dom to Header Menu

--- src/components/TipHeader.jsx
+++ src/components/TipHeader.jsx
@@ -1,5 +1,6 @@
 import { Col, Layout, Menu, Row } from 'antd';
 import logo from "../images/tipxmr-live.png";
+import { Link } from "react-router-dom"
 
 const { Header } = Layout
 
@@ -13,9 +14,9 @@ const TipHeader = () => {
         </Col>
         <Col>
           <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1">Streams</Menu.Item>
-            <Menu.Item key="2">Donate</Menu.Item>
-            <Menu.Item key="3">Login/Signup</Menu.Item>
+            <Menu.Item key="1"><Link to="/streamerpage">Streams</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="/donate">Donate</Link></Menu.Item>
+            <Menu.Item key="3"><Link to="/login">Login/Signup</Link></Menu.Item>
           </Menu>
         </Col>
       </Row>
