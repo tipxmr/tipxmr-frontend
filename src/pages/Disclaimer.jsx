3bfbe1922942932992fd76daaa7805915c2d28b3
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-12 14:01:25 +0200

    Remove all tailwind from disclaimer

--- src/pages/Disclaimer.jsx
+++ src/pages/Disclaimer.jsx
@@ -1,104 +1,85 @@
-import React from "react";
+import { Row, Col, Typography, Divider, List } from "antd"
 
-function Disclaimer() {
+const { Title, Link, Text } = Typography
+const tips = [{
+  title: "Tip 1: Keep your seedphrase save",
+  description: <div> Never EVER lose your seed phrase and keep it secret.These are the keys to your wallet, whoever has access can spend your money.Write the 25 words down on a <Link href='https://www.monero.how/monero-paper-wallet-offline-cold-storage' target='_blank'> piece of paper</Link> and store it securely.</div>,
+},
+{
+  title: "Tip 2: Keep your computer free of malware",
+  description: <div>It should go without saying, but just to be clear: If you use a compromised computer to log into the browser wallet, your funds are at risk of being stolen. Therefore, best take steps to ensure your computer is virus-free.</div>,
+},
+{
+  title: "Tip 3: For your other wallets, only use software endorsed by the Monero Project",
+  description: <div>There are many wallets out there and not all of them are honest. Before entering a seed into foreign software, check with the <Link href="https://getmonero.org/downloads">Monero website</Link> or <Link href="https://www.reddit.com/r/Monero/">Subreddit</Link> to see other people experience. We recommend using <Link href="https://cakewallet.com/">CakeWallet</Link> or <Link href="https://www.monerujo.io/">Monerujo</Link>.</div>
+},
+{
+  title: "Tip 4: Ensure SSL certificates when logging in",
+  description: <div>Before entering your seed into the browser on <Link href="https://tipxmr.live/">https://tipxmr.live/</Link> make sure that the address is indeed correct and that the website is served correctly by clicking the 🔒 lock icon.</div>
+}
+]
+
+const Disclaimer = () => {
   return (
-    <div className="w-3/4 my-3 mx-auto text-gray-200">
-      <div className="mx-auto my-8">
-        <h2 className="text-2xl text-center my-3">Introduction & Disclaimer</h2>
-        <p className="text-sm text-center">
-          Monero (XMR) is a cryptocurrency, in many ways similiar to Bitcoin.
-          However, the main difference to Bitcoin is that{" "}
-          <em>every transaction in Monero is private</em>. <br />
-          This means that the sender, reciever and the amount of ever
-          transaction are hidden with cryptographic techniques. While it is
-          fascinating to some to learn about how Monero works, it is not
-          required to be able to use it. Just like most drivers don't understand
-          the internals of the combustion engine.
-        </p>
-        <div className="border-4 border-xmrorange my-8 p-5 shadow-xl rounded text-center text-xmrorange-darker">
-          <p className="text-2xl text-center">Important:</p>
-          <p className="text-lg ">
-            At no point in time tipxmr.live has access to your Monero. We cannot
-            spend it and we cannot guarantee it's safety. While the Monero
-            protocol is very secure itself,{" "}
-            <span className="underline">your safety is your responsiblity</span>
-            .
-          </p>
-        </div>
-        <div className="my-2">
-          <h3 className="text-xl text-center underline my-3">
-            Tips for your safety:
-          </h3>
-          <ul className="list-decimal">
-            <li className="mb-3">
-              Never EVER lose your seed phrase and keep it secret. These are the
-              keys to your wallet, whoever has access can spend your money.
-              Write the 25 words down on a{" "}
-              <a
-                href="https://www.monero.how/monero-paper-wallet-offline-cold-storage"
-                target="_blank"
-                className="underline text-xmrorange"
-              >
-                piece of paper
-              </a>{" "}
-              and store it securely.
-            </li>
-            <li className="mb-3">
-              Make sure that your computer is not infected with malware. A
-              keylogger for example could read your seed when signing on.
-            </li>
-            <li className="mb-3">
-              Only use wallet software that is endorsed by the&nbsp;
-              <a
-                href="https.//getmonero.org/downloads"
-                className="underline text-xmrorange"
-                target="_blank"
-              >
-                Monero Project
-              </a>
-              , namely:
-              <ul className="list-disc w-5/6 mx-auto mt-3 text-sm">
-                <li>
-                  <a
-                    href="https.//getmonero.org/downloads"
-                    className="underline text-xmrorange text-base"
-                    target="_blank"
-                  >
-                    MyMonero
-                  </a>{" "}
-                  (iOS/Desktop/Browser)
-                </li>
-                <li>
-                  <a
-                    href="https.//getmonero.org/downloads"
-                    className="underline text-xmrorange text-base"
-                    target="_blank"
-                  >
-                    CakeWallet
-                  </a>{" "}
-                  (iOS/Android)
-                </li>
-                <li>
-                  <a
-                    href="https.//getmonero.org/downloads"
-                    className="underline text-xmrorange text-base"
-                    target="_blank"
-                  >
-                    Monerujo
-                  </a>{" "}
-                  (Android)
-                </li>
-              </ul>
-            </li>
-            <li className="mb-3">
-              When logging into tipxmr.live with your seed make sure that the
-              Website has a SSL-Certificate.
-            </li>
-            <li>Learn more about Monero!</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    <Row justify="center" align="center">
+      <Col span={24}>
+
+        {/* Heading */}
+        <Row justify="center" align="center">
+          <Col>
+            <Title level={1}>Introduction & Disclaimer</Title>
+          </Col>
+        </Row>
+
+        {/* About Monero */}
+        <Row justify="center" align="center" style={{ "text-align": "center" }}>
+          <Col span={8}>
+            <p>Monero (XMR) is a cryptocurrency, in many ways similiar to Bitcoin.
+          However, the main difference to Bitcoin is that{" "}<em>every transaction in Monero is private</em>.</p>
+
+            <Divider />
+
+            <p>This means that the sender, reciever and the amount of ever
+            transaction are hidden with cryptographic techniques. While it is
+            fascinating to some to learn about how Monero works, it is not
+            required to be able to use it. Just like most drivers don't understand
+            the internals of the combustion engine.</p>
+          </Col>
+        </Row>
+
+        {/* Important Box */}
+        <Row justify="center" align="center">
+          <Col span={10}>
+            <Divider />
+            <Title level={2}>
+              <Text underline>Important:</Text>
+            </Title>
+            <p>At no point in time tipxmr.live has access to your Monero. We cannot spend it and we cannot guarantee it's safety. While the Monero protocol is very secure itself, your safety is your responsiblity.</p>
+          </Col>
+        </Row>
+
+        {/* Safety Tipx */}
+        <Row justify="center" align="center">
+          <Col span={12}>
+            <Divider />
+            <Title level={2}>Tips for your safety:</Title>
+            <List
+              itemLayout="horizontal"
+              dataSource={tips}
+              renderItem={item => (
+                <List.Item>
+                  <List.Item.Meta
+                    title={<Title level={4}>{item.title}</Title>}
+                    description={<p>{item.description}</p>}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+        </Row>
+
+      </Col>
+    </Row>
   );
 }
 
