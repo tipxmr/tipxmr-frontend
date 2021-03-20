92bf25a50c45dd65361e4f531ecf9038a33801ec
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-20 14:37:36 +0200

    StreamerCard rework with antd

--- src/components/StreamerCard.jsx
+++ src/components/StreamerCard.jsx
@@ -1,21 +1,72 @@
-import React from "react";
 import PropTypes from "prop-types";
 import { FaTwitch, FaYoutube } from "react-icons/fa";
 import tipxmr from "../images/tipxmr-button.png";
 import { Link } from "react-router-dom";
+import { Row, Col, Card, Avatar, Image, Tag, Typography } from "antd";
+import {
+  EditOutlined,
+  EllipsisOutlined,
+  GiftOutlined,
+} from "@ant-design/icons";
 
+const { Meta } = Card;
+const { Title, Text } = Typography;
 const iconLookup = {
   youtube: FaYoutube,
   twitch: FaTwitch,
 };
 
-function StreamIcon({ type }) {
+const StreamIcon = ({ type }) => {
   const Icon = iconLookup[type];
-  return <Icon />;
-}
+  return <Icon style={{ display: "inline" }} />;
+};
 StreamIcon.propTypes = {
   type: PropTypes.oneOf(["youtube", "twitch"]),
 };
+
+const TipButton = ({ streamerName }) => {
+  return (
+    <Link to={"/donate/" + streamerName}>
+      <div style={{ margin: "0 auto" }}>
+        <Image src={tipxmr} width={100} preview={false} />
+      </div>
+    </Link>
+  );
+};
+TipButton.propTypes = {
+  streamerName: PropTypes.string,
+};
+
+const StreamerCard = ({ streamer }) => {
+  return (
+    <Card
+      cover={<img alt="example" src="https://i.imgur.com/PW3XO3u.jpg" />}
+      actions={[<TipButton streamerName={streamer.userName} />]}
+    >
+      <Meta
+        avatar={<Avatar src={streamer.profilePicture} />}
+        title={<Title level={2}>{streamer.displayName}</Title>}
+      />
+      <Row justify="center" align="middle" gutter={[12, 12]}>
+        <Col className="gutter-row">
+          <Tag>{streamer.stream.category}</Tag>
+        </Col>
+        <Col className="gutter-row">{streamer.stream.language}</Col>
+        <Col className="gutter-row">
+          <StreamIcon type={streamer.stream.platform} />
+        </Col>
+        <Col span={24} className="gutter-row" style={{ textAlign: "center" }}>
+          <Text type="secondary">{streamer.stream.description}</Text>
+        </Col>
+      </Row>
+    </Card>
+  );
+};
+StreamerCard.propTypes = {
+  streamer: PropTypes.object,
+};
+
+export default StreamerCard;
 
 // function Funding({ streamer }) {
 //   if (streamer) {
@@ -35,51 +86,3 @@ StreamIcon.propTypes = {
 // Funding.propTypes = {
 //   streamer: PropTypes.object,
 // };
-
-function StreamerCard({ streamer }) {
-  return (
-    <div className="mx-4">
-      <div className="max-w-xs bg-xmrgray-darker shadow-xl rounded-lg overflow-hidden">
-        <div className="flex flex-grow justify-center">
-          <img
-            className="object-cover object-center"
-            src="https://i.imgur.com/PW3XO3u.jpg"
-            alt="avatar"
-          />
-        </div>
-        <div className="flex flex-grow flex-col p-6">
-          <a href={streamer.stream.url}>
-            <div className="flex justify-center items-center transform hover:scale-110">
-              <StreamIcon type={streamer.stream.platform} />
-              <h1 className="text-2xl text-gray-200 mx-5">
-                {streamer.displayName}
-              </h1>
-              <p>{streamer.stream.language}</p>
-            </div>
-          </a>
-          <div className="mx-auto">
-            <span className="px-2 py-1 text-xs tracking-wide rounded-full border border-gray-200 text-gray-200">
-              #{streamer.stream.category}
-            </span>
-          </div>
-
-          <p className="p-2 truncate text-sm">{streamer.stream.description}</p>
-          {/* <Funding streamer={streamer} /> */}
-          <Link to={"/donate/" + streamer.userName}>
-            <img
-              src={tipxmr}
-              className="mx-auto h-auto w-32 shadow-md rounded transform hover:scale-110"
-            />
-          </Link>
-        </div>
-        <div className="text-center w-full mx-auto my-auto text-gray-200"></div>
-      </div>
-    </div>
-  );
-}
-
-StreamerCard.propTypes = {
-  streamer: PropTypes.object,
-};
-
-export default StreamerCard;
--- src/pages/StreamerPage.jsx
+++ src/pages/StreamerPage.jsx
@@ -1,10 +1,53 @@
 import { useEffect, useState } from "react";
-import { StreamerCard, CategoryNav } from "../components";
+import { StreamerCard } from "../components";
 import socketio from "../libs/socket_donator";
 import { Row, Col, Layout, Menu, Typography } from "antd";
 
 const { Sider, Content } = Layout;
 const { Title } = Typography;
+
+// For testing without a running backend
+const alex = {
+  _id: "fa80ac5814a6fddee2fa29a1e62f5de4e3a233f07a51e886a3a1e7a8bce5abf7",
+  animationSettings: {
+    bgImg: "",
+    charLimit: 99,
+    charPrice: 0.0004,
+    fontColor: "#FFFFFF",
+    fontShadow: true,
+    fontSize: "text-4xl",
+    gifs: true,
+    gifsMinAmount: 0,
+    goal: 100,
+    goalProgress: 0,
+    goalReached: false,
+    minAmount: 0,
+    secondPrice: 0.00042,
+    showGoal: true,
+    sound: "",
+  },
+  creationDate: new Date("2020-09-01"),
+  displayName: "AlexAnarcho",
+  donationStats: {
+    allDonations: [],
+    largestDonation: 0,
+    totalDonations: 0,
+  },
+  isOnline: false,
+  isPremium: true,
+  profilePicture:
+    "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
+  restoreHeight: 667580,
+  stream: {
+    category: "politics",
+    description: "I am a great streamer.",
+    language: "🇩🇪",
+    platform: "twitch",
+    url: "https://www.twitch.tv/n00bprogrammer",
+  },
+  streamerSocketId: "",
+  userName: "alexanarcho",
+};
 
 // TODO render the category page with a filter
 const StreamerPage = () => {
@@ -69,6 +112,9 @@ const StreamerPage = () => {
         <Row justify="center" style={{ paddingLeft: "5vw" }}>
           <Col span={24} style={{ textAlign: "center" }}>
             <Title level={1}>Online right now</Title>
+          </Col>
+          <Col span={6}>
+            <StreamerCard key={7} streamer={alex} />
           </Col>
         </Row>
       </Content>
