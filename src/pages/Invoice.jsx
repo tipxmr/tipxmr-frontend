e604242cc021467e299d62d8f53a5574b929b78f
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-21 12:31:06 +0200

    More tooltips and cleanup

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -1,6 +1,4 @@
 import { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import QR from "../images/test-qr.png";
 import monerojs from "../libs/monero";
 import {
   Spin,
@@ -18,7 +16,7 @@ import { WalletOutlined, InfoCircleOutlined } from "@ant-design/icons";
 
 const { Title, Text } = Typography;
 
-const Info = () => {
+const TierInfo = () => {
   const data = [
     <Text type="secondary">
       The Basic model will enable you to recieve XMR in livestreams. Great to
@@ -41,6 +39,10 @@ const Info = () => {
   );
 };
 
+const IntervalInfo = () => {
+  return <div>Pay for the whole year in advance, get one month for free!</div>;
+};
+
 const Payment = () => {
   const [yearly, setYearly] = useState(true);
   const [isPremium, setIsPremium] = useState(true);
@@ -60,10 +62,12 @@ const Payment = () => {
     e.nativeEvent.stopImmediatePropagation();
   };
 
+  // For testing
   const [subaddress, setSubaddress] = useState(
     "45ZoRheLkX2H3UjYSFs2wP9yo739nQ7irZA2pX6MQr5FeebkC2n8hABYGQRCcrzJ2AaGbNUyR4EfvanP1G2H5DSrMWi97Sk"
-  ); // will be props passed down
+  );
 
+  // Activate the TipXMR wallet and get subaddresses from it
   // useEffect(() => {
   //   if (subaddress === null) {
   //     getSubaddress();
@@ -104,7 +108,7 @@ const Payment = () => {
       <Row justify="center" gutter={[0, 24]}>
         <Col span={24}>
           Configure your membership for TipXMR{" "}
-          <Tooltip title={Info} placement="right">
+          <Tooltip title={TierInfo} placement="right">
             <InfoCircleOutlined />
           </Tooltip>
         </Col>
@@ -112,7 +116,9 @@ const Payment = () => {
           <Text>One Month</Text>
         </Col>
         <Col span={6}>
-          <Switch defaultChecked onChange={handleInterval} />
+          <Tooltip title={IntervalInfo}>
+            <Switch defaultChecked onChange={handleInterval} />
+          </Tooltip>
         </Col>
         <Col span={8}>
           <Text>One year</Text>
@@ -195,122 +201,12 @@ const Invoice = () => {
 
 export default Invoice;
 
-// TODO let the user choose between basic and premium
-// TODO integrate the subaddress from a different wallet
 // TODO confirmation on payment
 // TODO continue button one payment is confirmed
 // TODO integrate with backend (generate subaddress, confirm payment, confirm amount)
 // TODO update the db to the new date
 // TODO backend function to check if invoice is due on user login
 // TODO create sockets for subaddress getting
-const PaymentOld = () => {
-  // TODO props needed: subaddress, getSubaddress
-  const [yearly, setYearly] = useState(true);
-  const [monthly, setMonthly] = useState(false);
-  const [isPremium, setIsPremium] = useState(true);
-  const [amount, setAmount] = useState(0);
-
-  useEffect(() => {
-    const basePrice = isPremium ? 0.09 : 0.009;
-    const factor = yearly ? 11 : 1;
-    setAmount(basePrice * factor);
-  }, [monthly, yearly, isPremium]);
-
-  const activateLeft = () => {
-    setYearly(true);
-    setMonthly(false);
-  };
-
-  const activateRight = () => {
-    setYearly(false);
-    setMonthly(true);
-  };
-
-  const handleToggle = () => {
-    setIsPremium(!isPremium);
-  };
-
-  // payment handeling, work in progress
-  // Hard Coded for testing
-  const [subaddress, setSubaddress] = useState(
-    "45ZoRheLkX2H3UjYSFs2wP9yo739nQ7irZA2pX6MQr5FeebkC2n8hABYGQRCcrzJ2AaGbNUyR4EfvanP1G2H5DSrMWi97Sk"
-  ); // will be props passed down
-  const [qrCode, setQrCode] = useState(QR);
-  // const [paymentUri, setPaymentUri] = useState(null);
-
-  // useEffect(() => {
-  //   if (subaddress === null) {
-  //     getSubaddress();
-  //   }
-  // }, [getSubaddress, subaddress]);
-
-  // // generete QR Code on subaddress change
-  // useEffect(() => {
-  //   const paymentUri = createPaymentUri();
-  //   async function generateQrCode() {
-  //     if (subaddress !== null) {
-  //       const qrcode = await monerojs.generateQrCode(paymentUri);
-  //       setQrcode(qrcode);
-  //     }
-  //   }
-  //   generateQrCode();
-  // }, [subaddress, createPaymentUri]);
-
-  // function handleClick(e) {
-  //   e.stopPropagation();
-  //   e.nativeEvent.stopImmediatePropagation();
-  // }
-
-  // function createPaymentUri() {
-  //   let uri;
-  //   if (total > 0) {
-  //     uri = "monero:" + subaddress + "?tx_amount=" + total;
-  //   } else {
-  //     uri = "monero:" + subaddress;
-  //   }
-  //   setPaymentUri(uri);
-  //   return uri;
-  // }
-
-  return (
-    <div className="text-center">
-      <h1 className="mt-6 text-4xl text-center">tipxmr invoice is due</h1>
-      <p>
-        To keep using tipxmr for 1 {yearly ? "year" : "month"}, send{" "}
-        {amount.toFixed(4)} XMR to
-      </p>
-      <div className="flex justify-center mt-4">
-        <Button active={yearly} onClick={activateLeft}>
-          1 year
-        </Button>
-        <Button active={monthly} onClick={activateRight}>
-          1 month
-        </Button>
-      </div>
-      <div className="flex justify-center items-cente mt-3">
-        <Toggle isChecked={isPremium} onClick={handleToggle} />
-        <span className="mx-4 text-xmrgray-darker">Get Premium</span>
-      </div>
-      {/* <p className="tracking-tight text-sm"> */}
-      {/*   One year will get you one month for free */}
-      {/* </p> */}
-      <div>
-        <div className="flex justify-center my-6">
-          <img src={qrCode} alt="QR Code" />
-        </div>
-        <p className="text-xs">{subaddress}</p>
-      </div>
-      <div className="mt-4 border-t-2 border-dotted border-xmrgray-darker">
-        <p className="mt-2">
-          If you have the funds, you can pay directly with your tipxmr wallet
-        </p>
-        <div className="mt-2 flex justify-center">
-          <Button>Pay with tipxmr wallet</Button>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const Success = () => {
   return (
