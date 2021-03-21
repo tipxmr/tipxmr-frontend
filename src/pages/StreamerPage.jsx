44075af040f7c6fad83df0c4bff7d05c98d3f352
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-21 10:33:36 +0200

    Logic for rendering streamers in right category

--- src/pages/StreamerPage.jsx
+++ src/pages/StreamerPage.jsx
@@ -7,47 +7,49 @@ const { Sider, Content } = Layout;
 const { Title } = Typography;
 
 // For testing without a running backend
-const alex = {
-  _id: "fa80ac5814a6fddee2fa29a1e62f5de4e3a233f07a51e886a3a1e7a8bce5abf7",
-  animationSettings: {
-    bgImg: "",
-    charLimit: 99,
-    charPrice: 0.0004,
-    fontColor: "#FFFFFF",
-    fontShadow: true,
-    fontSize: "text-4xl",
-    gifs: true,
-    gifsMinAmount: 0,
-    goal: 100,
-    goalProgress: 0,
-    goalReached: false,
-    minAmount: 0,
-    secondPrice: 0.00042,
-    showGoal: true,
-    sound: "",
+const alex = [
+  {
+    _id: "fa80ac5814a6fddee2fa29a1e62f5de4e3a233f07a51e886a3a1e7a8bce5abf7",
+    animationSettings: {
+      bgImg: "",
+      charLimit: 99,
+      charPrice: 0.0004,
+      fontColor: "#FFFFFF",
+      fontShadow: true,
+      fontSize: "text-4xl",
+      gifs: true,
+      gifsMinAmount: 0,
+      goal: 100,
+      goalProgress: 0,
+      goalReached: false,
+      minAmount: 0,
+      secondPrice: 0.00042,
+      showGoal: true,
+      sound: "",
+    },
+    creationDate: new Date("2020-09-01"),
+    displayName: "AlexAnarcho",
+    donationStats: {
+      allDonations: [],
+      largestDonation: 0,
+      totalDonations: 0,
+    },
+    isOnline: false,
+    isPremium: true,
+    profilePicture:
+      "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
+    restoreHeight: 667580,
+    stream: {
+      category: "politics",
+      description: "I am a great streamer.",
+      language: "🇩🇪",
+      platform: "twitch",
+      url: "https://www.twitch.tv/n00bprogrammer",
+    },
+    streamerSocketId: "",
+    userName: "alexanarcho",
   },
-  creationDate: new Date("2020-09-01"),
-  displayName: "AlexAnarcho",
-  donationStats: {
-    allDonations: [],
-    largestDonation: 0,
-    totalDonations: 0,
-  },
-  isOnline: false,
-  isPremium: true,
-  profilePicture:
-    "data:image/gif;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNk+A8AAQUBAScY42YAAAAASUVORK5CYII=",
-  restoreHeight: 667580,
-  stream: {
-    category: "politics",
-    description: "I am a great streamer.",
-    language: "🇩🇪",
-    platform: "twitch",
-    url: "https://www.twitch.tv/n00bprogrammer",
-  },
-  streamerSocketId: "",
-  userName: "alexanarcho",
-};
+];
 
 const StreamerPage = () => {
   const [activeCategory, setActiveCategory] = useState("all");
@@ -60,13 +62,13 @@ const StreamerPage = () => {
     socketio.onGetOnlineStreamer(setOnlineStreamers);
   }, []);
 
-  const handleCategory = (e) => {
+  const handleActiveCategory = (e) => {
     setActiveCategory(e.key);
   };
 
   const RenderStreamerCards = () => {
-    if (onlineStreamers && onlineStreamers.value) {
-      return onlineStreamers.value.map((streamer) => {
+    if (onlineStreamers) {
+      return onlineStreamers.map((streamer) => {
         if (activeCategory === streamer.stream.category) {
           return (
             <StreamerCard key={streamer.displayName} streamer={streamer} />
@@ -79,7 +81,6 @@ const StreamerPage = () => {
       });
     }
     return null;
-    // return <StreamerCard key={7} streamer={alex} />;
   };
 
   return (
@@ -94,7 +95,7 @@ const StreamerPage = () => {
           defaultSelectedKeys={["all"]}
           mode="inline"
           style={{ fontSize: "1.5rem" }}
-          onClick={handleCategory}
+          onClick={handleActiveCategory}
         >
           <Menu.Item key="all">All</Menu.Item>
           <Menu.Item key="gaming">Gaming</Menu.Item>
