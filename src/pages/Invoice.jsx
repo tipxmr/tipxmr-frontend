cb2634dea92136898204a7fa8551eb47ca7385d3
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-21 13:23:13 +0200

    Modal as component

--- src/pages/Invoice.jsx
+++ src/pages/Invoice.jsx
@@ -104,97 +104,93 @@ const Payment = () => {
   }, [yearly, isPremium]);
 
   return (
-    <div>
-      <Row justify="center" gutter={[0, 24]}>
-        <Col span={24}>
-          Configure your membership for TipXMR{" "}
-          <Tooltip title={TierInfo} placement="right">
-            <InfoCircleOutlined />
-          </Tooltip>
-        </Col>
-        <Col span={8}>
-          <Text>One Month</Text>
-        </Col>
-        <Col span={6}>
-          <Tooltip title={IntervalInfo}>
-            <Switch defaultChecked onChange={handleInterval} />
-          </Tooltip>
-        </Col>
-        <Col span={8}>
-          <Text>One year</Text>
-        </Col>
-        <Col span={8}>
-          <Text>Basic</Text>
-        </Col>
-        <Col span={6}>
-          <Switch defaultChecked onChange={handleTier} />
-        </Col>
-        <Col span={8}>
-          <Text>Premium</Text>
-        </Col>
-      </Row>
-      <Divider />
-      <Row justify="center" align="middle">
-        <Col span={24}>
-          <img
-            src={qrCode}
-            alt="qr code"
-            preview={false}
-            style={{ margin: "0 auto" }}
-          />
-        </Col>
-        <Col>
-          <a href={paymentUri} onClick={handleClick}>
-            <WalletOutlined /> Pay from desktop wallet
-          </a>
-        </Col>
-      </Row>
-      <Divider />
-      <Row justify="center" style={{ textAlign: "center" }}>
-        <Col span={24}>
-          <Title level={3}>Total: {amount} XMR</Title>
-        </Col>
-      </Row>
-    </div>
+    <Row justify="center" align="middle">
+      <Col span={24}>
+        <Row justify="center" gutter={[0, 24]}>
+          <Col span={24}>
+            Configure your membership for TipXMR{" "}
+            <Tooltip title={TierInfo} placement="right">
+              <InfoCircleOutlined />
+            </Tooltip>
+          </Col>
+          <Col span={8}>
+            <Text>One Month</Text>
+          </Col>
+          <Col span={6}>
+            <Tooltip title={IntervalInfo}>
+              <Switch defaultChecked onChange={handleInterval} />
+            </Tooltip>
+          </Col>
+          <Col span={8}>
+            <Text>One year</Text>
+          </Col>
+          <Col span={8}>
+            <Text>Basic</Text>
+          </Col>
+          <Col span={6}>
+            <Switch defaultChecked onChange={handleTier} />
+          </Col>
+          <Col span={8}>
+            <Text>Premium</Text>
+          </Col>
+        </Row>
+        <Divider />
+        <Row justify="center" align="middle">
+          <Col span={24}>
+            <img
+              src={qrCode}
+              alt="qr code"
+              preview={false}
+              style={{ margin: "0 auto" }}
+            />
+          </Col>
+          <Col>
+            <a href={paymentUri} onClick={handleClick}>
+              <WalletOutlined /> Pay from desktop wallet
+            </a>
+          </Col>
+        </Row>
+        <Divider />
+        <Row justify="center" style={{ textAlign: "center" }}>
+          <Col span={24}>
+            <Title level={3}>Total: {amount} XMR</Title>
+          </Col>
+        </Row>
+      </Col>
+    </Row>
   );
 };
 
 const InvoiceModal = () => {
-  Modal.info({
-    title: <Title level={2}>Invoice</Title>,
-    content: <Payment />,
-    okButtonProps: {
-      disabled: true,
-      icon: <Spin size="small" />,
-      size: "large",
-    },
-    okText: "Awaiting Payment",
-  });
-};
-
-const SuccessModal = () => {
-  Modal.success({
-    title: <Title level={2}>Thank you for using TipXMR</Title>,
-    content: (
-      <Row justify="center">
-        <Col span={24}></Col>
-      </Row>
-    ),
-  });
-};
-
-const Invoice = () => {
   const [isPayed, setIsPayed] = useState(false);
   return (
-    <div className="flex flex-grow self-center justify-center">
-      <Button onClick={InvoiceModal}>InvoiceModal</Button>
-      <Button onClick={SuccessModal}>SuccessModal</Button>
+    <Modal
+      visible
+      centered
+      title={<Title level={2}>Invoice</Title>}
+      closable={false}
+      cancelButtonProps={{ visible: "false" }}
+      footer={[
+        <Button
+          type="primary"
+          disabled={!isPayed}
+          icon={!isPayed ? <Spin size="small" /> : null}
+          size="large"
+        >
+          {isPayed ? "Continue" : "Awaiting Payment"}
+        </Button>,
+      ]}
+    >
+      <Payment />
+    </Modal>
+  );
+};
 
-      {/* <div className="bg-gray-200 text-xmrgray-darker rounded"> */}
-      {/*   <div className="rounded border-4 border-dashed border-xmrorange p-10 m-6"> */}
-      {/*     {isPayed ? <Success /> : <Payment />} */}
-      {/*   </div> */}
-      {/* </div> */}
+// For testing
+const Invoice = () => {
+  return (
+    <div>
+      <InvoiceModal />
     </div>
   );
 };
@@ -207,16 +203,3 @@ export default Invoice;
 // TODO update the db to the new date
 // TODO backend function to check if invoice is due on user login
 // TODO create sockets for subaddress getting
-
-const Success = () => {
-  return (
-    <div className="text-center text-xmrgray-darker">
-      <h1 className="text-4xl">We got your payment!</h1>
-      <h2 className="text-4xl">🤙✅🙏</h2>
-      <h2 className="mt-4 text-2xl">Thank you for using our service!</h2>
-      <div className="flex justify-center mt-6 pt-6 border-t-2 border-dashed border-xmrgray-darker">
-        <Button>Continue to dashboard</Button>
-      </div>
-    </div>
-  );
-};
