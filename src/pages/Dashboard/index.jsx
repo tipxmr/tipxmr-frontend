629a0cc9ad301e737797f823a10cb2bbfbbcd3c3
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-22 12:15:15 +0200

    Prettier

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useRouteMatch, Route, Link, Redirect } from "react-router-dom";
 import monerologo from "../../images/monero-symbol.png";
 import Overview from "./Overview";
