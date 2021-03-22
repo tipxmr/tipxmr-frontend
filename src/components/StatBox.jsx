a9df208435aeb81ac01d72ca642bc02804912738
AlexAnarcho <alexanarcho@protonmail.com>
2021-03-22 13:43:21 +0200

    Clean up StatBox

--- src/components/StatBox.jsx
+++ src/components/StatBox.jsx
@@ -1,8 +1,7 @@
-import React from "react";
 import PropTypes from "prop-types";
 import clsx from "clsx";
 
-function StatBox({ boxTitle, boxStat, textSize = "text-md" }) {
+const StatBox = ({ boxTitle, boxStat, textSize = "text-md" }) => {
   const boxStyles = clsx([
     "rounded",
     "shadow-lg",
