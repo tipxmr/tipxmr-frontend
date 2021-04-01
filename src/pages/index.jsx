76e838d7f4bf2514a13589ed5d4bd28b5249a1bd
AlexAnarcho <alexanarcho@protonmail.com>
2021-04-01 18:05:23 +0300

    Removed categories, since uneccessary

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,6 @@ export { default as Donate } from "./Donate";
 export { default as Animation } from "./Animation";
 export { default as Dashboard } from "./Dashboard";
 export { default as StreamerPage } from "./StreamerPage";
-export { default as Categories } from "./Categories";
 export { default as NotFound404 } from "./NotFound404";
 export { default as Logout } from "./Logout";
 export { default as Landing } from "./Landing";
